bde63a3a13462272230a1200391d0f43aca36e5d
igustavo11 <131629449+igustavo11@users.noreply.github.com>
2025-04-06 14:23:13 -0300

    chore: initial commit with project setup

---
+++ components/sidebar/sidebar.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import * as React from "react";
+import { Avatar, AvatarImage } from "../ui/avatar";
+import home from "../../public/images/home-line.svg";
+import iconavatar from "../../public/images/avatar.png";
+import logo from "../../public/images/logoCadastra.svg";
+import Image from "next/image";
+import clientes from "../../public/images/clientes.svg";
+
+export default function Sidebar() {
+  return (
+    <div className="flex h-screen w-64 flex-col justify-between bg-white p-4 shadow-md">
+      <div className="space-y-6 ml-1 font-semibold ">
+        <div className="flex h-16 items-center justify-center">
+          <Image src={logo} alt="Logo" />
+        </div>
+
+        <div className="space-y-2 ">
+          <div className="flex items-center gap-3  rounded-md px-3 py-2 text-base text-gray-500 font-semibold hover:bg-card-background hover:text-primary">
+            <Image src={home} alt="home" />
+            <span>Home</span>
+          </div>
+
+          <div className="flex items-center gap-3 rounded-md px-3 py-2 text-base font-semibold bg-card-background text-primary">
+            <Image src={clientes} alt="clientes" />
+            <span>Clientes</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="border-t border-slate-100 pt-4">
+        <div className="flex items-center gap-3 px-3 py-2">
+          <Avatar>
+            <AvatarImage src={iconavatar.src} alt="Avatar do usuário" />
+          </Avatar>
+          <div className="text-sm">
+            <p className="font-medium">Fernanda de Souza</p>
+            <p className="text-gray-500">fernanda.souza</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FF7400",
+        "card-background": "#fff0e0",
+        border: "var(--border)",
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+        secondary: "#FF4D00",
+        textcard: "#3F3F3F",
+        iconcard: "#F9F9F9",
+      },
+    },
+  },
+  plugins: [],
+};
